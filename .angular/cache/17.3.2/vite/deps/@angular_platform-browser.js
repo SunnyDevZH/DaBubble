b08d4ca6efbb9c077eaff9a4c47e22d29b09c365
Yannick Vaterlaus <yannick.vaterlaus@windowslive.com>
2024-06-30 22:20:55 +0200

    update

---
+++ .angular/cache/17.3.2/vite/deps/@angular_platform-browser.js
@@ -0,0 +1,88 @@
+import {
+  BrowserDomAdapter,
+  BrowserGetTestability,
+  BrowserModule,
+  By,
+  DomEventsPlugin,
+  DomRendererFactory2,
+  DomSanitizer,
+  DomSanitizerImpl,
+  EVENT_MANAGER_PLUGINS,
+  EventManager,
+  EventManagerPlugin,
+  HAMMER_GESTURE_CONFIG,
+  HAMMER_LOADER,
+  HammerGestureConfig,
+  HammerGesturesPlugin,
+  HammerModule,
+  HydrationFeatureKind,
+  INTERNAL_BROWSER_PLATFORM_PROVIDERS,
+  KeyEventsPlugin,
+  Meta,
+  REMOVE_STYLES_ON_COMPONENT_DESTROY,
+  SharedStylesHost,
+  Title,
+  TransferState,
+  VERSION,
+  bootstrapApplication,
+  createApplication,
+  disableDebugTools,
+  enableDebugTools,
+  initDomAdapter,
+  makeStateKey,
+  platformBrowser,
+  provideClientHydration,
+  provideProtractorTestingSupport,
+  withHttpTransferCacheOptions,
+  withNoHttpTransferCache
+} from "./chunk-KD6ZQLZR.js";
+import "./chunk-TC4SRICU.js";
+import {
+  getDOM
+} from "./chunk-AEMON3VG.js";
+import "./chunk-PQ4ACPVX.js";
+import "./chunk-PWORDH67.js";
+import "./chunk-KWBPU7EF.js";
+import "./chunk-AQUUAMGA.js";
+import "./chunk-LHRTKJE6.js";
+import "./chunk-LJ4VCL4A.js";
+export {
+  BrowserModule,
+  By,
+  DomSanitizer,
+  EVENT_MANAGER_PLUGINS,
+  EventManager,
+  EventManagerPlugin,
+  HAMMER_GESTURE_CONFIG,
+  HAMMER_LOADER,
+  HammerGestureConfig,
+  HammerModule,
+  HydrationFeatureKind,
+  Meta,
+  REMOVE_STYLES_ON_COMPONENT_DESTROY,
+  Title,
+  TransferState,
+  VERSION,
+  bootstrapApplication,
+  createApplication,
+  disableDebugTools,
+  enableDebugTools,
+  makeStateKey,
+  platformBrowser,
+  provideClientHydration,
+  provideProtractorTestingSupport,
+  withHttpTransferCacheOptions,
+  withNoHttpTransferCache,
+  BrowserDomAdapter as ɵBrowserDomAdapter,
+  BrowserGetTestability as ɵBrowserGetTestability,
+  DomEventsPlugin as ɵDomEventsPlugin,
+  DomRendererFactory2 as ɵDomRendererFactory2,
+  DomSanitizerImpl as ɵDomSanitizerImpl,
+  HammerGesturesPlugin as ɵHammerGesturesPlugin,
+  INTERNAL_BROWSER_PLATFORM_PROVIDERS as ɵINTERNAL_BROWSER_PLATFORM_PROVIDERS,
+  KeyEventsPlugin as ɵKeyEventsPlugin,
+  SharedStylesHost as ɵSharedStylesHost,
+  getDOM as ɵgetDOM,
+  initDomAdapter as ɵinitDomAdapter
+};
+//# sourceMappingURL=@angular_platform-browser.js.map
--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyB51b2MdG_fyKroGxWt1gaTFFM1PQPQAWM",
-    authDomain: "da-bubble-ca3ba.firebaseapp.com",
-    projectId: "da-bubble-ca3ba",
-    storageBucket: "da-bubble-ca3ba.appspot.com",
-    messagingSenderId: "385480205423",
-    appId: "1:385480205423:web:b9780b36e122b1c662999a"
-  };
+  apiKey: "AIzaSyALf-Gcl9BdcbRn9Sugpdd_M24kIG5Ttdk",
+  authDomain: "da-bubble-yannick.firebaseapp.com",
+  projectId: "da-bubble-yannick",
+  storageBucket: "da-bubble-yannick.appspot.com",
+  messagingSenderId: "219603892438",
+  appId: "1:219603892438:web:e5e76795ce08ab337eb177"
+};
