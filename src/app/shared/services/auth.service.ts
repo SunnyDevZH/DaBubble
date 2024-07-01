ced244e1d4e4c5bd40f0b916ce626086e8f4e6f5
Yannick Vaterlaus <yannick.vaterlaus@windowslive.com>
2024-07-01 16:39:23 +0200

    update

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -216,7 +216,7 @@ export class AuthService {
                 await updateProfile(response.user, {
                     displayName: `Gast #${randomUserNumber}`,
                     photoURL:
-                        "http://localhost:4200/assets/img/characters/template" +
+                        "http://yannick-vaterlaus.ch/dabubble/assets/img/characters/template" +
                         randomInt +
                         ".svg",
                 });
